b5055996459bc6079301277213852b59aaa87dea
Tyler Rajotte <tylerrajotte@Tylers-Mac-mini.local>
2021-05-04 10:42:22 -0600

    Forgot some extra bits

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (function() {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: /Users/tylerrajotte/Desktop/mapeitechsheets/src/index.js: Support for the experimental syntax 'jsx' isn't currently enabled (8:3):\\n\\n\\u001b[0m \\u001b[90m  6 |\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m  7 |\\u001b[39m \\u001b[33mReactDOM\\u001b[39m\\u001b[33m.\\u001b[39mrender(\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m  8 |\\u001b[39m   \\u001b[33m<\\u001b[39m\\u001b[33mApp\\u001b[39m \\u001b[33m/\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[33m,\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m    |\\u001b[39m   \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m  9 |\\u001b[39m   document\\u001b[33m.\\u001b[39mgetElementById(\\u001b[32m'root'\\u001b[39m)\\u001b[0m\\n\\u001b[0m \\u001b[90m 10 |\\u001b[39m )\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\nAdd @babel/preset-react (https://git.io/JfeDR) to the 'presets' section of your Babel config to enable transformation.\\nIf you want to leave it as-is, add @babel/plugin-syntax-jsx (https://git.io/vb4yA) to the 'plugins' section to enable parsing.\\n    at Parser._raise (/Users/tylerrajotte/Desktop/mapeitechsheets/node_modules/@babel/parser/lib/index.js:807:17)\\n    at Parser.raiseWithData (/Users/tylerrajotte/Desktop/mapeitechsheets/node_modules/@babel/parser/lib/index.js:800:17)\\n    at Parser.expectOnePlugin (/Users/tylerrajotte/Desktop/mapeitechsheets/node_modules/@babel/parser/lib/index.js:9946:18)\\n    at Parser.parseExprAtom (/Users/tylerrajotte/Desktop/mapeitechsheets/node_modules/@babel/parser/lib/index.js:11327:22)\\n    at Parser.parseExprSubscripts (/Users/tylerrajotte/Desktop/mapeitechsheets/node_modules/@babel/parser/lib/index.js:10904:23)\\n    at Parser.parseUpdate (/Users/tylerrajotte/Desktop/mapeitechsheets/node_modules/@babel/parser/lib/index.js:10884:21)\\n    at Parser.parseMaybeUnary (/Users/tylerrajotte/Desktop/mapeitechsheets/node_modules/@babel/parser/lib/index.js:10862:23)\\n    at Parser.parseExprOps (/Users/tylerrajotte/Desktop/mapeitechsheets/node_modules/@babel/parser/lib/index.js:10719:23)\\n    at Parser.parseMaybeConditional (/Users/tylerrajotte/Desktop/mapeitechsheets/node_modules/@babel/parser/lib/index.js:10693:23)\\n    at Parser.parseMaybeAssign (/Users/tylerrajotte/Desktop/mapeitechsheets/node_modules/@babel/parser/lib/index.js:10656:21)\");\n\n//# sourceURL=webpack://mapeitechsheets/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
